f0ea4f450b661cbce91a0894e8660b583d1b12cf
endalk200 <eb808826@gmail.com>
2024-07-17 16:35:25 +0300

    feat: added starter shell script which will be used as a starter session configuration file

---
+++ starter.sh
@@ -0,0 +1,20 @@
+. ./util/functions.sh
+
+project_root ~/src/my_project
+session_name "my_project"
+
+# first, create your session
+new_session
+
+# then, layout your session
+rename_window "editor"
+run_command "nvim"
+
+new_window "servers"
+split_horizontal 50%
+
+# at the end, select the window you want first
+select_window "editor"
+
+# then, attach to your session!
+attach_to_session
